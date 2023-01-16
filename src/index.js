6daa4d167e78a956b743b342ed2ae3c1073f6de0
aswinina <116088497+aswinina@users.noreply.github.com>
2023-01-16 17:33:45 +0100

    update the weather code

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { StrictMode } from "react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import "bootstrap/dist/css/bootstrap.css";
+import "./index.css";
+import "./App.css";
+import Weather from "./Weather";
+import reportWebVitals from "./reportWebVitals";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <StrictMode>
+    <div className="Weather">
+      <Weather />
+      <span className="sourcecode">
+        <a href="https://github.com/aswinina/weather-react">
+          Open-source code
+        </a>
+        by Aswini Sureddy
+      </span>
+    </div>
+  </StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/weather.js
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+import axios from "axios";
+import "./styles.css";
+export default function Weather() {
+  const [temperature, setTemperature] = useState("");
+  const [description, setDescription] = useState("");
+  const [humidity, setHumidity] = useState("");
+  const [wind, setWind] = useState("");
+  const [city, setCity] = useState("");
+  const [icon, setIcon] = useState("");
+  let form = (
+    <div>
+      <div className="Weather">
+        <div className="container">
+          <div className="row">
+            <div className="offset-sm-1 col-sm-10">
+              <form onSubmit={handleSubmit}>
+                <div class="mb-3 location">
+                  <label for="exampleInputEmail1" class="form-label"></label>
+                  <input
+                    type="text"
+                    placeholder="Enter location"
+                    class="form-control"
+                    autoComplete="off"
+                    onChange={updateCity}
+                  />
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+
+  function showTemperature(response) {
+    setTemperature(Math.round(response.data.main.temp));
+    setDescription(response.data.weather[0].description);
+    setHumidity(response.data.main.humidity);
+    setWind(Math.round(response.data.wind.speed));
+    setIcon(
+      `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    );
+  }
+  function handleSubmit(event) {
+    event.preventDefault();
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=5b74d10f3ef03caf1ac640b557c288c3&units=metric`;
+    axios.get(url).then(showTemperature);
+  }
+
+  function changeCity(event) {
+    setCity(event.target.value);
+  }
+  return (
+    <div>
+      <h1>Weather Engine</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="search" onChange={changeCity} />
+        <input type="submit" value="Search" />
+      </form>
+      <ul>
+        <li>Temperature: {temperature}°C</li>
+        <li>Description: {description}</li>
+        <li>Humidity: {humidity}%</li>
+        <li>Wind: {wind} km/h</li>
+        <li>
+          <img src={icon} />
+        </li>
+      </ul>
+    </div>
+  );
+}
